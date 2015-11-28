baf3318489b058e02f24c5835d181720834b2a3c
Graham McGregor <dev@grahammcgregor.ca>
2015-11-28 16:55:21 -0500

    Text msg logs

--- AndroidChat/app/src/main/java/cisc434/androidchat/ChatActivity.java
+++ AndroidChat/app/src/main/java/cisc434/androidchat/ChatActivity.java
@@ -1,17 +1,20 @@
 package cisc434.androidchat;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,6 +34,21 @@ public class ChatActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        EditText edit_txt = (EditText) findViewById(R.id.txtMessage);
+        ListView chat_msgs = (ListView) findViewById(R.id.viewChatMessages);
+
+        edit_txt.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event ) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    Log.w("chatApp", v.getText().toString());
+                    v.setText("");
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
